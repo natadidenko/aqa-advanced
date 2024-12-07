d59dd5e58a2d213da45545220cdbc2ffbdf1189d
Natalka Didenko <natalkadidenko@MacBook-Air-Natalka.local>
2024-12-07 12:49:45 +0200

    Add example with chalk

---
+++ nodeBasics/index.js
@@ -0,0 +1,5 @@
+const chalk = require('chalk');
+
+console.log(chalk.bold.green('Жирний текст зеленого кольору.'));
+console.log(chalk.blue('Текст синього кольору.'));
+console.log(chalk.bold.red('Жирний червоний текст.'));
