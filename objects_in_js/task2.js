1d9c51fd2af917767bdbe8f62b9a382ca8cd032c
Natalka Didenko <natalkadidenko@MacBook-Air-Natalka.local>
2024-12-27 20:57:27 +0200

    Create and manipulate objects and arrays with properties, using operations like destructuring, adding, and deleting properties, then log the results.

---
+++ objects_in_js/task2.js
@@ -0,0 +1,10 @@
+let book = {
+    title: "Павло Казарін",
+    author: "Дикий Захід Східної Європи",
+    year: 2021
+  };
+
+let { title, author } = book;
+  
+console.log("Назва книги:", title);
+console.log("Автор книги:", author);
